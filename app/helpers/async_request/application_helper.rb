e39a44a43a2d3a1097407cc970427813580caa77
Derek Dannevig <46461968+DDannevig@users.noreply.github.com>
2022-12-12 09:30:50 -0300

    [UG-450] extra_data implementation (#1)
    * exta_data implementation
    
    * WiP tests
    
    * Erase specs
    
    * Suggestion
    
    * Suggestion

--- app/helpers/async_request/application_helper.rb
+++ app/helpers/async_request/application_helper.rb
@@ -1,15 +1,43 @@
 module AsyncRequest
   module ApplicationHelper
-    def execute_async(worker_class, *params)
+    def execute_async(worker_class, *worker_params)
       raise ArgumentError if worker_class.nil?
+      @worker_params = worker_params
+      @worker_class = worker_class
       job = Job.create!(
         worker: worker_class,
-        params: params,
+        params: set_worker_params,
         status: Job.statuses[:waiting],
         uid: SecureRandom.uuid
       )
       JobProcessor.perform_async(job.id, job.worker)
       job.uid
+    end
+
+    def set_extra_data(new_data)
+      @extra_data = new_data
+    end
+
+    def add_extra_data(new_data)
+      extra_data.merge!(new_data)
+    end
+
+    private
+
+    def extra_data
+      @extra_data ||= {}
+    end
+
+    def set_worker_params 
+      send_extra_data? ? params_and_extra_data : @worker_params
+    end
+
+    def send_extra_data?
+      extra_data.present? && @worker_class.respond_to?(:handle_extra_data?) && @worker_class.handle_extra_data?
+    end
+
+    def params_and_extra_data
+      @worker_params.push(extra_data.merge(extra_data: true))
     end
   end
 end
